732dd2c5cf6b55b7ff5eb74d68a06fbe4a5aa07f
Emmanuel Nkrumah-Sarpong <nkrumahthis@gmail.com>
2024-01-07 20:39:41 +0000

    create ssh session

--- main.go
+++ main.go
@@ -80,4 +80,6 @@ func main() {
 	if err != nil {
 		panic(err.Error())
 	}
+
+	CreateSSHSession(connection)
 }
--- ssh.go
+++ ssh.go
@@ -55,9 +55,87 @@ func CreateServerConnection(server Server) (*ssh.Client, error) {
 	return conn, nil
 }
 
+func CreateSSHSession(conn *ssh.Client) {
+	fmt.Println("Creating a client session for the connection")
+	var session *ssh.Session
+	var err error
+
+	session, err = conn.NewSession()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	defer session.Close()
+
+	var stdin io.WriteCloser
+	var stdout, stderr io.Reader
+
+	stdin, err = session.StdinPipe()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	stdout, err = session.StdoutPipe()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	stderr, err = session.StderrPipe()
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+
+	wr := make(chan []byte, 10)
+
+	go func() {
+		for {
+			select {
+			case d := <-wr:
+				_, err := stdin.Write(d)
+				if err != nil {
+					fmt.Println(err.Error())
+				}
+			}
+		}
+	}()
+
+	go func() {
+		scanner := bufio.NewScanner(stdout)
+		for {
+			if tkn := scanner.Scan(); tkn {
+				rcv := scanner.Bytes()
+				raw := make([]byte, len(rcv))
+				copy(raw, rcv)
+				fmt.Println(string(raw))
+			} else {
+				if scanner.Err() != nil {
+					fmt.Println(scanner.Err())
+				} else {
+					fmt.Println("io.EOF")
+				}
+				return
+			}
+		}
+	}()
+
+	go func() {
+		scanner := bufio.NewScanner(stderr)
+		for scanner.Scan() {
+			fmt.Println(scanner.Text())
+		}
+	}()
+
+	session.Shell()
+
+	for {
+		fmt.Println("$")
+		scanner := bufio.NewScanner(os.Stdin)
+		scanner.Scan()
+		text := scanner.Text()
+		wr <- []byte(text + "\n")
+	}
+}
 
 // from https://medium.com/@marcus.murray/go-ssh-client-shell-session-c4d40daa46cd
-
 func ConnectToServer(host, user, pwd string) {
 	pKey, err := os.ReadFile("config/privatekey")
 	if err != nil {
