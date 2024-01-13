f96d9fbc4accca6c1392fd68f736633cd4b74be6
Emmanuel Nkrumah-Sarpong <nkrumahthis@gmail.com>
2024-01-13 23:14:43 +0000

    change concurrency logs

--- concurrency/main.go
+++ concurrency/main.go
@@ -6,23 +6,24 @@ import (
 )
 
 func main() {
-	fmt.Println("started c")
+	fmt.Println("started main")
 	channel := make(chan string)
 
 	go waitC(channel)
-	time.Sleep(time.Second * 3)
+	time.Sleep(time.Second * 1)
+	fmt.Println("main finished")
 
 	var response string
 	response = <- channel
-	
+
 	fmt.Println(response)
 	fmt.Println("end")
 
 }
 
 func waitC(channel chan string) {
+	fmt.Println("started waitC")
 	time.Sleep(time.Second * 3)
-	fmt.Println("C")
-	response := "ok done"
+	response := "waitC finished"
 	channel <- response
 }
